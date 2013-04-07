536f62f76634936cd45d1511b52e3d27b7c5b698
Karthik Chandraraj <ckarthik17@gmail.com>
2013-04-07 14:06:07 +0530

    CK : id added and log removed

--- src/main/java/com/amnesty/panicbutton/SettingsActivity.java
+++ src/main/java/com/amnesty/panicbutton/SettingsActivity.java
@@ -3,7 +3,6 @@ package com.amnesty.panicbutton;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
 public class SettingsActivity extends Activity {
@@ -12,7 +11,6 @@ public class SettingsActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-		Log.i(TAG, "onCreate");
         setContentView(R.layout.settings);
     }
 
