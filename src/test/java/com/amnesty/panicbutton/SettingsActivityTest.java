14f49e4c122b64a20e808ff9225470bc279e4661
Karthik Chandraraj <ckarthik17@gmail.com>
2013-04-07 14:06:45 +0530

    CK : #1 test added for launching SMS activity

--- src/test/java/com/amnesty/panicbutton/SettingsActivityTest.java
+++ src/test/java/com/amnesty/panicbutton/SettingsActivityTest.java
@@ -1,17 +1,37 @@
 package com.amnesty.panicbutton;
 
-import com.xtremelabs.robolectric.RobolectricTestRunner;
+import android.content.Intent;
+import android.widget.TableRow;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.shadows.ShadowActivity;
 
-import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
+import static org.robolectric.Robolectric.shadowOf;
 
 @RunWith(RobolectricTestRunner.class)
 public class SettingsActivityTest {
+    private SettingsActivity settingsActivity;
+    private TableRow smsRow;
+
+    @Before
+    public void setUp() {
+        settingsActivity = new SettingsActivity();
+        settingsActivity.onCreate(null);
+
+        smsRow = (TableRow) settingsActivity.findViewById(R.id.sms_row);
+    }
+
     @Test
-    public void shouldHaveHelloWorld(){
-        String appName = new SettingsActivity().getResources().getString(R.string.app_name);
-        assertThat(appName, equalTo("PanicButton"));
+    public void pressingTheSMSRowShouldStartTheSMSConfigActivity() throws Exception {
+        smsRow.performClick();
+
+        ShadowActivity shadowActivity = shadowOf(settingsActivity);
+        Intent startedIntent = shadowActivity.getNextStartedActivity();
+
+        assertThat(startedIntent.getComponent().getClassName(), equalTo(SMSConfigActivity.class.getName()));
     }
 }
