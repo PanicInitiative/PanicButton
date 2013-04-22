222516011e84176d9aee47359c5ca111167dec98
Sriram Narasimhan <sriram.iyengar78@gmail.com>
2013-04-22 13:32:04 +0530

    Sriram\CK: Story:24: adding triggers class

---
+++ src/test/java/com/amnesty/panicbutton/trigger/TriggersTest.java
@@ -0,0 +1,33 @@
+package com.amnesty.panicbutton.trigger;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class TriggersTest {
+    private Triggers triggers;
+    private long currentTime;
+
+    @Before
+    public void setUp() {
+        triggers = new Triggers();
+        currentTime = System.currentTimeMillis();
+    }
+
+    @Test
+    public void shouldBeActiveWhenEventsAreAddedWithTimeLimit() {
+        for (int i = 0; i < 5; i++) {
+            triggers.add(currentTime + (i * 500));
+        }
+        assertTrue(triggers.isActive());
+    }
+
+    @Test
+    public void shouldBeInActiveWhenEventsAreAddedOutsideTimeLimit() {
+        triggers.add(currentTime);
+        triggers.add(currentTime + 5500);
+        assertFalse(triggers.isActive());
+    }
+}
