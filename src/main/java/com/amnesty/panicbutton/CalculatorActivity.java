8235d6a8b4efbb708eb095ae4c0f21248bbc2911
Sriram Narasimhan <sriram.iyengar78@gmail.com>
2013-04-22 18:08:39 +0530

    CK\Sriram : start hardware trigger service on apllication start

--- src/main/java/com/amnesty/panicbutton/CalculatorActivity.java
+++ src/main/java/com/amnesty/panicbutton/CalculatorActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import com.amnesty.panicbutton.trigger.HardwareTriggerService;
 import roboguice.activity.RoboActivity;
 import roboguice.inject.ContentView;
 
@@ -13,6 +14,7 @@ public class CalculatorActivity extends RoboActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         Button equalsButton = (Button) findViewById(R.id.equals_sign);
         equalsButton.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
@@ -21,5 +23,7 @@ public class CalculatorActivity extends RoboActivity {
                 return true;
             }
         });
+
+        startService(new Intent(this, HardwareTriggerService.class));
     }
 }
--- src/test/java/com/amnesty/panicbutton/CalculatorActivityTest.java
+++ src/test/java/com/amnesty/panicbutton/CalculatorActivityTest.java
@@ -2,13 +2,16 @@ package com.amnesty.panicbutton;
 
 import android.content.Intent;
 import android.widget.Button;
+import com.amnesty.panicbutton.trigger.HardwareTriggerService;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.shadows.ShadowActivity;
+import org.robolectric.shadows.ShadowIntent;
 
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 import static org.robolectric.Robolectric.shadowOf;
 
 @RunWith(RobolectricTestRunner.class)
@@ -16,16 +19,28 @@ public class CalculatorActivityTest {
     private CalculatorActivity calculatorActivity;
     private Button equalsButton;
 
-    @Test
-    public void shouldNavigateToSettingsScreenOnLongPress() {
+    @Before
+    public void setUp() {
         calculatorActivity = new CalculatorActivity();
         calculatorActivity.onCreate(null);
-        equalsButton = (Button) calculatorActivity.findViewById(R.id.equals_sign);
+    }
 
+    @Test
+    public void shouldNavigateToSettingsScreenOnLongPress() {
+        equalsButton = (Button) calculatorActivity.findViewById(R.id.equals_sign);
         equalsButton.performLongClick();
 
         ShadowActivity shadowActivity = shadowOf(calculatorActivity);
         Intent startedIntent = shadowActivity.getNextStartedActivity();
         assertThat(startedIntent.getComponent().getClassName(), equalTo(SettingsActivity.class.getName()));
+    }
+
+    @Test
+    public void shouldStartHardwareTriggerServiceOnApplicationCreation() {
+        ShadowActivity shadowActivity = shadowOf(calculatorActivity);
+        Intent startedIntent = shadowActivity.getNextStartedService();
+        ShadowIntent shadowIntent = shadowOf(startedIntent);
+        assertNotNull(startedIntent);
+        assertEquals(HardwareTriggerService.class, shadowIntent.getIntentClass());
     }
 }
