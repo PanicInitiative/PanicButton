5a55281e7a196bf6271b5f57383d07fe054fbb52
Karthik Chandraraj <ckarthik17@gmail.com>
2013-04-24 18:41:36 +0530

    CK\Sriram : #11 Trigger alert and settings from facade for all buttons

--- src/main/java/com/amnesty/panicbutton/CalculatorActivity.java
+++ src/main/java/com/amnesty/panicbutton/CalculatorActivity.java
@@ -12,37 +12,47 @@ import roboguice.inject.ContentView;
 
 @ContentView(R.layout.calculator_layout)
 public class CalculatorActivity extends RoboActivity {
+    private int[] buttons = {R.id.one, R.id.two, R.id.three, R.id.four, R.id.five, R.id.six, R.id.seven, R.id.eight,
+                             R.id.nine, R.id.zero, R.id.equals_sign, R.id.plus, R.id.minus, R.id.multiply, R.id.divide};
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        bindLongPress();
+        registerButtonEvents();
         startService(new Intent(this, HardwareTriggerService.class));
     }
 
-    private void bindLongPress() {
-        Button equalsButton = (Button) findViewById(R.id.equals_sign);
-        equalsButton.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View view) {
-                startActivity(new Intent(CalculatorActivity.this, SettingsActivity.class));
-                overridePendingTransition(R.anim.show_from_bottom, R.anim.hide_to_top);
-                return true;
-            }
-        });
+    private void registerButtonEvents() {
+        for(int button : buttons) {
+            Button equalsButton = (Button) findViewById(button);
+            equalsButton.setOnLongClickListener(longClickListener);
+            equalsButton.setOnClickListener(clickListener);
+        }
     }
 
-    public void onButtonClick(View view) {
-        MultiClickEvent multiClickEvent = (MultiClickEvent) view.getTag();
-        if (multiClickEvent == null) {
-            multiClickEvent = resetEvent(view);
+    private View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            MultiClickEvent multiClickEvent = (MultiClickEvent) view.getTag();
+            if (multiClickEvent == null) {
+                multiClickEvent = resetEvent(view);
+            }
+            multiClickEvent.registerClick(System.currentTimeMillis());
+            if (multiClickEvent.isActivated()) {
+                getPanicAlert().start();
+                resetEvent(view);
+            }
         }
-        multiClickEvent.registerClick(System.currentTimeMillis());
-        if (multiClickEvent.isActivated()) {
-            getPanicAlert().start();
-            resetEvent(view);
+    };
+
+    private View.OnLongClickListener longClickListener = new View.OnLongClickListener() {
+        @Override
+        public boolean onLongClick(View view) {
+            startActivity(new Intent(CalculatorActivity.this, SettingsActivity.class));
+            overridePendingTransition(R.anim.show_from_bottom, R.anim.hide_to_top);
+            return true;
         }
-    }
+    };
 
     private MultiClickEvent resetEvent(View view) {
         MultiClickEvent multiClickEvent = new MultiClickEvent();
