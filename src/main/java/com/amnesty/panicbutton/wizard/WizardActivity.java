080289787f6913e9916fd13d159138d02f085f70
Karthik Chandraraj <ckarthik17@gmail.com>
2013-04-25 18:01:25 +0530

    CK\Sriram : #46 wizard next, previous button wired to pager

--- src/main/java/com/amnesty/panicbutton/wizard/WizardActivity.java
+++ src/main/java/com/amnesty/panicbutton/wizard/WizardActivity.java
@@ -1,13 +1,42 @@
 package com.amnesty.panicbutton.wizard;
 
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.widget.Button;
 import com.amnesty.panicbutton.R;
-import roboguice.activity.RoboActivity;
+import roboguice.activity.RoboFragmentActivity;
 import roboguice.inject.ContentView;
+import roboguice.inject.InjectView;
+
+import static android.support.v4.view.ViewPager.SimpleOnPageChangeListener;
 
 @ContentView(R.layout.wizard_layout)
-public class WizardActivity extends RoboActivity {
+public class WizardActivity extends RoboFragmentActivity {
+    private ViewPagerWithoutSwipe viewPager;
+    @InjectView(R.id.previous_button) Button previousButton;
+
+    private SimpleOnPageChangeListener pageChangeListener = new ViewPager.SimpleOnPageChangeListener() {
+        @Override
+        public void onPageSelected(int position) {
+            super.onPageSelected(position);
+            previousButton.setEnabled(position != 0);
+        }
+    };
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        viewPager = (ViewPagerWithoutSwipe) findViewById(R.id.pager);
+        previousButton.setEnabled(false);
+        viewPager.setOnPageChangeListener(pageChangeListener);
+    }
+
+    public void next(View view) {
+        viewPager.setCurrentItem(viewPager.getCurrentItem() + 1);
+    }
+
+    public void previous(View view) {
+        viewPager.setCurrentItem(viewPager.getCurrentItem() - 1);
     }
 }
--- src/test/java/com/amnesty/panicbutton/wizard/WizardActivityTest.java
+++ src/test/java/com/amnesty/panicbutton/wizard/WizardActivityTest.java
@@ -1,26 +1,70 @@
 package com.amnesty.panicbutton.wizard;
 
+import android.widget.Button;
 import com.amnesty.panicbutton.R;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import static org.robolectric.Robolectric.shadowOf;
 
 @RunWith(RobolectricTestRunner.class)
 public class WizardActivityTest {
     private WizardActivity wizardActivity;
+    private Button previousButton;
+    private Button nextButton;
+    private ViewPagerWithoutSwipe viewPager;
 
     @Before
     public void setUp() {
         wizardActivity = new WizardActivity();
         wizardActivity.onCreate(null);
+
+        previousButton = (Button) wizardActivity.findViewById(R.id.previous_button);
+        nextButton = (Button) wizardActivity.findViewById(R.id.next_button);
+        viewPager = (ViewPagerWithoutSwipe) wizardActivity.findViewById(R.id.pager);
     }
 
     @Test
     public void shouldLoadTheWizardLayoutOnCreate() {
         assertEquals(R.id.wizard_layout_root, shadowOf(wizardActivity).getContentView().getId());
+    }
+
+    @Test
+    public void shouldHavePreviousDisabledOnFirstScreen() {
+        assertFalse(previousButton.isEnabled());
+        assertEquals(0, viewPager.getCurrentItem());
+    }
+
+    @Test
+    public void shouldNavigateToNextScreenAndEnablePreviousButton() {
+        nextButton.performClick();
+        assertEquals(1, viewPager.getCurrentItem());
+        assertTrue(previousButton.isEnabled());
+    }
+
+    @Test
+    public void shouldHavePreviousDisabledOnNavigatingBackToFirstScreen() {
+        nextButton.performClick();
+        previousButton.performClick();
+        assertFalse(previousButton.isEnabled());
+    }
+
+    @Test
+    public void shouldNavigateToPreviousScreen() {
+        nextButton.performClick();
+        nextButton.performClick();
+
+        previousButton.performClick();
+        assertEquals(1, viewPager.getCurrentItem());
+    }
+
+    @Test
+    @Ignore
+    public void shouldHaveNextDisabledOnLastScreen() {
+        assertFalse(nextButton.isEnabled());
     }
 }
