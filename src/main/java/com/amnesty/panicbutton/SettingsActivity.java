b8605615e9febfac4793cc6939811d914830d02d
Sriram Narasimhan <sriram.iyengar78@gmail.com>
2013-05-06 15:32:37 +0530

    CK/Sriram: Defect#59

--- src/main/java/com/amnesty/panicbutton/SettingsActivity.java
+++ src/main/java/com/amnesty/panicbutton/SettingsActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import com.amnesty.panicbutton.alert.PanicAlert;
 import com.amnesty.panicbutton.model.SMSSettings;
 import com.amnesty.panicbutton.sms.SMSSettingsActivity;
@@ -15,6 +16,8 @@ import roboguice.inject.InjectView;
 public class SettingsActivity extends RoboActivity {
     @InjectView(R.id.activate_alert)
     private Button activateButton;
+    @InjectView(R.id.alert_status_text)
+    private TextView alertStatusText;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,12 +27,17 @@ public class SettingsActivity extends RoboActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        initActivateButton();
+        setAlertStatus();
     }
 
-    private void initActivateButton() {
+    private void setAlertStatus() {
         SMSSettings smsSettings = SMSSettings.retrieve(this);
         activateButton.setEnabled(smsSettings.isConfigured());
+        if(smsSettings.isConfigured()) {
+            alertStatusText.setText(R.string.alert_status_standby);
+        } else {
+            alertStatusText.setText(R.string.alert_status_error);
+        }
     }
 
     public void launchSmsActivity(View view) {
