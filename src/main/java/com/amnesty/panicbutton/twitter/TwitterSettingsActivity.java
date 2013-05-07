60c4399c8c3db1538c6c128a5623a28d07cd642a
Karthik Chandraraj <ckarthik17@gmail.com>
2013-05-07 17:31:47 +0530

    CK\Sriram : #19 Toggle twitter alert

--- src/main/java/com/amnesty/panicbutton/twitter/TwitterSettingsActivity.java
+++ src/main/java/com/amnesty/panicbutton/twitter/TwitterSettingsActivity.java
@@ -1,12 +1,33 @@
 package com.amnesty.panicbutton.twitter;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Spinner;
 import com.amnesty.panicbutton.R;
 import roboguice.activity.RoboFragmentActivity;
+import roboguice.inject.InjectView;
+
+import static android.view.View.INVISIBLE;
+import static android.view.View.VISIBLE;
 
 public class TwitterSettingsActivity extends RoboFragmentActivity {
+    @InjectView(R.id.country_spinner)
+    private Spinner countrySpinner;
+
+    @InjectView(R.id.phone_service_spinner)
+    private Spinner phoneServiceSpinner;
+
+    @InjectView(R.id.opt_twitter_checkbox)
+    private CheckBox optTwitterCheckbox;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.twitter_settings_layout);
+    }
+
+    public void toggleCountryAndService(View view) {
+        countrySpinner.setVisibility(optTwitterCheckbox.isChecked() ? VISIBLE : INVISIBLE);
+        phoneServiceSpinner.setVisibility(optTwitterCheckbox.isChecked() ? VISIBLE : INVISIBLE);
     }
 }
--- src/test/java/com/amnesty/panicbutton/twitter/TwitterSettingsActivityTest.java
+++ src/test/java/com/amnesty/panicbutton/twitter/TwitterSettingsActivityTest.java
@@ -1,26 +1,52 @@
 package com.amnesty.panicbutton.twitter;
 
+import android.widget.CheckBox;
+import android.widget.Spinner;
 import com.amnesty.panicbutton.R;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import static org.robolectric.Robolectric.shadowOf;
 
 @RunWith(RobolectricTestRunner.class)
 public class TwitterSettingsActivityTest {
     private TwitterSettingsActivity twitterSettingsActivity;
+    private CheckBox optTwitterCheckbox;
+    private Spinner countrySpinner;
+    private Spinner phoneServiceSpinner;
 
     @Before
     public void setup() {
         twitterSettingsActivity = new TwitterSettingsActivity();
         twitterSettingsActivity.onCreate(null);
+
+        optTwitterCheckbox = (CheckBox) twitterSettingsActivity.findViewById(R.id.opt_twitter_checkbox);
+        countrySpinner = (Spinner) twitterSettingsActivity.findViewById(R.id.country_spinner);
+        phoneServiceSpinner = (Spinner) twitterSettingsActivity.findViewById(R.id.phone_service_spinner);
     }
 
     @Test
     public void shouldLoadTwitterLayoutOnCreate() {
         assertEquals(R.id.twitter_settings_layout_root, shadowOf(twitterSettingsActivity).getContentView().getId());
+        assertFalse(countrySpinner.isShown());
+        assertFalse(phoneServiceSpinner.isShown());
+    }
+
+    @Test
+    public void shouldShowCountryAndPhoneServiceListOnEnablingTwitterAlert() {
+        optTwitterCheckbox.performClick();
+        assertTrue(countrySpinner.isShown());
+        assertTrue(phoneServiceSpinner.isShown());
+    }
+
+    @Test
+    public void shouldHideCountryAndPhoneServiceListOnDisablingTwitterAlert() {
+        optTwitterCheckbox.setChecked(true);
+        optTwitterCheckbox.performClick();
+        assertFalse(countrySpinner.isShown());
+        assertFalse(phoneServiceSpinner.isShown());
     }
 }
