32f2ccaf2fd8edde4f5417865ddc37f12edacf20
Sriram Narasimhan <sriram.iyengar78@gmail.com>
2013-05-09 18:07:20 +0530

    CK/Sriram: #19 Hide or show shortcode based on settings status

--- src/main/java/com/amnesty/panicbutton/twitter/TwitterSettingsActivity.java
+++ src/main/java/com/amnesty/panicbutton/twitter/TwitterSettingsActivity.java
@@ -1,10 +1,15 @@
 package com.amnesty.panicbutton.twitter;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.CheckBox;
 import com.amnesty.panicbutton.R;
 import roboguice.activity.RoboFragmentActivity;
 import roboguice.inject.InjectView;
+
+import static android.view.View.INVISIBLE;
+import static android.view.View.VISIBLE;
 
 public class TwitterSettingsActivity extends RoboFragmentActivity {
 
@@ -12,6 +17,13 @@ public class TwitterSettingsActivity extends RoboFragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.twitter_settings_layout);
     }
+
+    public void toggleTwitterSettings(View view) {
+        shortCodeLayout.setVisibility(optTwitterCheckbox.isChecked() ? VISIBLE : INVISIBLE);
+    }
+
+    @InjectView(R.id.twitter_short_code_layout)
+    private ViewGroup shortCodeLayout;
 
     @InjectView(R.id.opt_twitter_checkbox)
     private CheckBox optTwitterCheckbox;
--- src/test/java/com/amnesty/panicbutton/twitter/TwitterSettingsActivityTest.java
+++ src/test/java/com/amnesty/panicbutton/twitter/TwitterSettingsActivityTest.java
@@ -1,5 +1,6 @@
 package com.amnesty.panicbutton.twitter;
 
+import android.view.ViewGroup;
 import android.widget.CheckBox;
 import com.amnesty.panicbutton.R;
 import org.junit.Before;
@@ -7,24 +8,45 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import static org.robolectric.Robolectric.shadowOf;
 
 @RunWith(RobolectricTestRunner.class)
 public class TwitterSettingsActivityTest {
     private TwitterSettingsActivity twitterSettingsActivity;
     private CheckBox optTwitterCheckbox;
+    private ViewGroup shortCodeLayout;
+    private ViewGroup messageLayout;
 
     @Before
     public void setup() {
         twitterSettingsActivity = new TwitterSettingsActivity();
         twitterSettingsActivity.onCreate(null);
+
         optTwitterCheckbox = (CheckBox) twitterSettingsActivity.findViewById(R.id.opt_twitter_checkbox);
+        shortCodeLayout = (ViewGroup) twitterSettingsActivity.findViewById(R.id.twitter_short_code_layout);
+        messageLayout = (ViewGroup) twitterSettingsActivity.findViewById(R.id.twitter_message_layout);
     }
 
     @Test
     public void shouldLoadTwitterLayoutOnCreate() {
         assertEquals(R.id.twitter_settings_layout_root, shadowOf(twitterSettingsActivity).getContentView().getId());
+        assertFalse(shortCodeLayout.isShown());
+        assertFalse(messageLayout.isShown());
+    }
+
+    @Test
+    public void shouldShowShortCodeFragmentOnEnablingTwitter() {
+        optTwitterCheckbox.performClick();
+        assertTrue(shortCodeLayout.isShown());
+    }
+
+    @Test
+    public void shouldHideShortCodeFragmentAndMessageFragmentOnDisablingTwitter() {
+        optTwitterCheckbox.setChecked(true);
+        optTwitterCheckbox.performClick();
+        assertFalse(shortCodeLayout.isShown());
+        assertFalse(messageLayout.isShown());
     }
 }
 
