028f1caaa8fa4361fcb6dc75f13fdaa625de8b14
Karthik Chandraraj <ckarthik17@gmail.com>
2013-05-13 10:45:50 +0530

    CK #19 : Header and Max Characters made configurable in MessageFragment

--- src/main/java/com/amnesty/panicbutton/common/MessageFragment.java
+++ src/main/java/com/amnesty/panicbutton/common/MessageFragment.java
@@ -1,7 +1,10 @@
 package com.amnesty.panicbutton.common;
 
+import android.app.Activity;
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.text.InputFilter;
+import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,14 +17,28 @@ import roboguice.inject.InjectView;
 import static java.lang.String.valueOf;
 
 public class MessageFragment extends RoboFragment {
-    private static final int MAX_CHARACTER_COUNT = 85;
     @InjectView(R.id.characters_left_view)
     private TextView charactersLeftView;
+
+    @InjectView(R.id.message_fragment_header)
+    private TextView messageHeaderView;
 
     @InjectView(R.id.message_edit_text)
     private EditText messageEditText;
 
     private MessageLimitWatcher messageLimitWatcher;
+    private int maxCharacters;
+    private String messageHeader;
+
+    @Override
+    public void onInflate(Activity activity, AttributeSet attrs, Bundle savedInstanceState) {
+        super.onInflate(activity, attrs, savedInstanceState);
+
+        TypedArray typedArray = activity.obtainStyledAttributes(attrs, R.styleable.MessageFragmentArguments);
+        maxCharacters = typedArray.getInt(R.styleable.MessageFragmentArguments_max_characters, -1);
+        messageHeader = typedArray.getString(R.styleable.MessageFragmentArguments_message_header);
+        typedArray.recycle();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -33,12 +50,13 @@ public class MessageFragment extends RoboFragment {
         super.onViewCreated(view, savedInstanceState);
         String prefix = getString(R.string.characters_left);
 
-        messageLimitWatcher = new MessageLimitWatcher(charactersLeftView, prefix, MAX_CHARACTER_COUNT);
+        messageLimitWatcher = new MessageLimitWatcher(charactersLeftView, prefix, maxCharacters);
         messageEditText.addTextChangedListener(messageLimitWatcher);
-        InputFilter[] filters = {new InputFilter.LengthFilter(MAX_CHARACTER_COUNT)};
+        InputFilter[] filters = {new InputFilter.LengthFilter(maxCharacters)};
         messageEditText.setFilters(filters);
 
-        charactersLeftView.setText(prefix + valueOf(MAX_CHARACTER_COUNT - messageEditText.getText().length()));
+        charactersLeftView.setText(prefix + valueOf(maxCharacters - messageEditText.getText().length()));
+        messageHeaderView.setText(messageHeader);
     }
 
     public String getMessage() {
