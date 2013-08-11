b188b13abdd536b8d4d690f9881ac674b8ba0606
sohjsolwin <sohjsolwin@gmail.com>
2013-08-11 08:42:04 -0400

    Added Guardian mode to watch your back when you need it.

--- src/main/java/com/apb/beacon/SettingsActivity.java
+++ src/main/java/com/apb/beacon/SettingsActivity.java
@@ -1,16 +1,20 @@
 package com.apb.beacon;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import com.apb.beacon.alert.AlertStatus;
 import com.apb.beacon.alert.PanicAlert;
 import com.apb.beacon.dialer.DialerSettingsActivity;
+import com.apb.beacon.guardian.GuardianSettingsActivity;
 import com.apb.beacon.sms.SMSSettingsActivity;
 import com.apb.beacon.twitter.TwitterSettingsActivity;
-import roboguice.activity.RoboActivity;
+
 import roboguice.inject.ContentView;
 import roboguice.inject.InjectView;
 
@@ -18,6 +22,8 @@ import roboguice.inject.InjectView;
 public class SettingsActivity extends PanicButtonActivity {
     @InjectView(R.id.activate_alert)
     private Button activateButton;
+    @InjectView(R.id.activate_guardian)
+    private Button guardianButton;
     @InjectView(R.id.alert_status_text)
     private TextView alertStatusText;
     @InjectView(R.id.dialer_row)
@@ -26,10 +32,16 @@ public class SettingsActivity extends PanicButtonActivity {
     private TextView smsSettingsLink;
     @InjectView(R.id.twitter_row)
     private TextView twitterSettingsLink;
+    @InjectView(R.id.guardian_row)
+    private TextView guardianSettingsLink;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+//        if (savedInstanceState.getBoolean("ACTIVATE_ONLY", false)) {
+//            killGuardian();
+//        }
     }
 
     @Override
@@ -50,6 +62,10 @@ public class SettingsActivity extends PanicButtonActivity {
         startActivity(new Intent(this, TwitterSettingsActivity.class));
     }
 
+    public void launchGuardianActivity(View view) {
+        startActivity(new Intent(this, GuardianSettingsActivity.class));
+    }
+
     public void goBack(View view) {
         this.finish();
     }
@@ -59,10 +75,56 @@ public class SettingsActivity extends PanicButtonActivity {
         updateView();
     }
 
+    public void performGuardianAction(View view) {
+
+        String alarm = view.getContext().ALARM_SERVICE;
+        AlarmManager am = ( AlarmManager ) getSystemService( alarm );
+
+        Intent intent = new Intent( "com.apb.beacon.ACTIVATE_ALERT_ACTION" );
+        PendingIntent pi = PendingIntent.getBroadcast( this, 0, intent, 0 );
+
+        if (ApplicationSettings.isGuardianActive(view.getContext())) {
+            am.cancel(pi);
+            ApplicationSettings.setIsGuardianActive(view.getContext(), false);
+        } else {
+
+            ApplicationSettings.setIsGuardianActive(view.getContext(), true);
+
+            int type = AlarmManager.ELAPSED_REALTIME_WAKEUP;
+            long interval = ApplicationSettings.getGuardianTime(view.getContext()) * 1000;
+            long triggerTime = SystemClock.elapsedRealtime() + interval;
+
+            am.set(type, triggerTime, pi );
+
+        }
+        updateView();
+    }
+//
+//    public void killGuardian(){
+//
+//        String alarm = this.ALARM_SERVICE;
+//        AlarmManager am = ( AlarmManager ) getSystemService( alarm );
+//
+//        Intent intent = new Intent( "com.apb.beacon.ACTIVATE_ALERT_ACTION" );
+//        PendingIntent pi = PendingIntent.getBroadcast( this, 0, intent, 0 );
+//        am.cancel(pi);
+//        ApplicationSettings.setIsGuardianActive(this, false);
+//        SettingsActivity.this.finish();
+//    }
+
     private void toggleAlert() {
         PanicAlert panicAlert = getPanicAlert();
         if(panicAlert.isActive()) {
             panicAlert.deActivate();
+
+            String alarm = this.getBaseContext().ALARM_SERVICE;
+            AlarmManager am = ( AlarmManager ) getSystemService( alarm );
+
+            Intent intent = new Intent( "com.apb.beacon.ACTIVATE_ALERT_ACTION" );
+            PendingIntent pi = PendingIntent.getBroadcast( this, 0, intent, 0 );
+
+            am.cancel(pi);
+
             return;
         }
         SettingsActivity.this.finish();
@@ -72,10 +134,21 @@ public class SettingsActivity extends PanicButtonActivity {
     private void updateView() {
         AlertStatus alertStatus = getPanicAlert().getAlertStatus();
         activateButton.setText(alertStatus.getAction());
+        String guardianMessage;
+        if (!ApplicationSettings.isGuardianActive(this.getBaseContext())) {
+            guardianMessage = getString(R.string.guardian_inactive) + " ("
+                    + ApplicationSettings.getGuardianTime(this.getBaseContext()).toString() + ")";
+        } else {
+            guardianMessage = getString(R.string.guardian_active);
+        }
+        guardianButton.setText(guardianMessage);
+        guardianButton.setEnabled(!ApplicationSettings.isAlertActive(this.getBaseContext()));
         activateButton.setBackgroundResource(alertStatus.getStyle());
         alertStatusText.setText(alertStatus.getDescription());
+        dialerSettingsLink.setEnabled(alertStatus.isSettingsEnabled());
         smsSettingsLink.setEnabled(alertStatus.isSettingsEnabled());
         twitterSettingsLink.setEnabled(alertStatus.isSettingsEnabled());
+        guardianSettingsLink.setEnabled(alertStatus.isSettingsEnabled());
         updateAlertStatusStrip();
     }
 }
--- src/main/java/com/apb/beacon/common/Intents.java
+++ src/main/java/com/apb/beacon/common/Intents.java
@@ -10,6 +10,7 @@ public class Intents {
     private static final String PREFIX = "com.apb.beacon";
     public static final String LOCATION_UPDATE_ACTION = PREFIX + ".LOCATION_UPDATE_ACTION";
     public static final String SEND_ALERT_ACTION = PREFIX + ".SEND_ALERT_ACTION";
+    public static final String ACTIVATE_ALERT_ACTION = PREFIX + ".ACTIVATE_ALERT_ACTION";
 
     public static PendingIntent alarmPendingIntent(Context context) {
         return PendingIntent.getBroadcast(context, 0, new Intent(SEND_ALERT_ACTION), FLAG_UPDATE_CURRENT);
