edbea2c2e1bf1eab1e5a2f5ecdeb26907b057a13
Jun Matsushita <jun@iilab.org>
2014-06-10 21:47:18 +0200

    Added logging for location provider

--- src/main/java/com/apb/beacon/location/CurrentLocationProvider.java
+++ src/main/java/com/apb/beacon/location/CurrentLocationProvider.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.location.Location;
 import android.location.LocationManager;
 import android.location.Criteria;
+import android.util.Log;
 
 import static android.location.Criteria.ACCURACY_FINE;
 import static android.location.LocationManager.NETWORK_PROVIDER;
@@ -27,10 +28,13 @@ public class CurrentLocationProvider extends LocationListenerAdapter {
           bestLocation = null;
         Location latestLocation = getLatest(bestLocation,
             manager.getLastKnownLocation(GPS_PROVIDER));
+        Log.e("CurrentLocationProvider", "latestlocation GPS_PROVIDER = " + latestLocation);
         latestLocation = getLatest(latestLocation,
             manager.getLastKnownLocation(NETWORK_PROVIDER));
+        Log.e("CurrentLocationProvider", "latestlocation NETWORK_PROVIDER = " + latestLocation);
         latestLocation = getLatest(latestLocation,
             manager.getLastKnownLocation(PASSIVE_PROVIDER));
+        Log.e("CurrentLocationProvider", "latestlocation PASSIVE_PROVIDER = " + latestLocation);
         currentLocation = latestLocation;
         if (currentLocation != null) 
         	ApplicationSettings.setCurrentBestLocation(context, currentLocation);
