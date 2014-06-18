54613ee7d14962a4738e871fdc6cf8cd3094b692
khobaib <khobaib@gmail.com>
2014-06-18 16:30:58 +0600

    PB-756 #comment Fixed problem with security tips link crash fixes #30

--- src/main/java/org/iilab/pb/common/CustomLinkMovementMethod.java
+++ src/main/java/org/iilab/pb/common/CustomLinkMovementMethod.java
@@ -1,11 +1,18 @@
 package org.iilab.pb.common;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.text.Layout;
 import android.text.Spannable;
 import android.text.method.LinkMovementMethod;
+import android.text.method.MovementMethod;
+import android.text.style.URLSpan;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
@@ -17,65 +24,46 @@ public class CustomLinkMovementMethod extends LinkMovementMethod {
 
     private static CustomLinkMovementMethod linkMovementMethod = new CustomLinkMovementMethod();
 
-    @Override
-    public boolean onTouchEvent( TextView widget, Spannable buffer, MotionEvent event ) {
-        try {
-            return super.onTouchEvent( widget, buffer, event ) ;
-        } catch( Exception ex ) {
-            Log.e(">>>>>>", "INVALID link");
+    public boolean onTouchEvent(TextView widget, Spannable buffer, MotionEvent event) {
+        int action = event.getAction();
 
-//            Toast.makeText(MainActivity.this, "Could not load link", Toast.LENGTH_LONG).show();
-            return true;
+        if (action == MotionEvent.ACTION_UP) {
+            int x = (int) event.getX();
+            int y = (int) event.getY();
+
+            x -= widget.getTotalPaddingLeft();
+            y -= widget.getTotalPaddingTop();
+
+            x += widget.getScrollX();
+            y += widget.getScrollY();
+
+            Layout layout = widget.getLayout();
+            int line = layout.getLineForVertical(y);
+            int off = layout.getOffsetForHorizontal(line, x);
+
+            URLSpan[] link = buffer.getSpans(off, off, URLSpan.class);
+            if (link.length != 0) {
+                String url = link[0].getURL();
+                if (url.contains("https") || url.contains("http")) {
+                    Log.d("Link", url);
+                    Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                    movementContext.startActivity(browserIntent);
+                    return true;
+                } else {
+                    Log.d("Link", url);
+                    widget.setHighlightColor(Color.WHITE);
+                    Toast.makeText(movementContext, "Invalid link.", Toast.LENGTH_LONG).show();
+                    return false;
+                }
+
+            }
         }
+
+        return super.onTouchEvent(widget, buffer, event);
     }
 
-//    public boolean onTouchEvent(android.widget.TextView widget, android.text.Spannable buffer, android.view.MotionEvent event)
-//    {
-//        int action = event.getAction();
-//
-//        if (action == MotionEvent.ACTION_UP)
-//        {
-//            int x = (int) event.getX();
-//            int y = (int) event.getY();
-//
-//            x -= widget.getTotalPaddingLeft();
-//            y -= widget.getTotalPaddingTop();
-//
-//            x += widget.getScrollX();
-//            y += widget.getScrollY();
-//
-//            Layout layout = widget.getLayout();
-//            int line = layout.getLineForVertical(y);
-//            int off = layout.getOffsetForHorizontal(line, x);
-//
-//            URLSpan[] link = buffer.getSpans(off, off, URLSpan.class);
-//            if (link.length != 0)
-//            {
-//                String url = link[0].getURL();
-//                if (url.contains("https"))
-//                {
-//                    Log.d("Link", url);
-//                    Toast.makeText(movementContext, "Link was clicked", Toast.LENGTH_LONG).show();
-//                } else if (url.contains("tel"))
-//                {
-//                    Log.d("Link", url);
-//                    Toast.makeText(movementContext, "Tel was clicked", Toast.LENGTH_LONG).show();
-//                } else if (url.contains("mailto"))
-//                {
-//                    Log.d("Link", url);
-//                    Toast.makeText(movementContext, "Mail link was clicked", Toast.LENGTH_LONG).show();
-//                }
-//                return true;
-//            }
-//        }
-//
-//        return super.onTouchEvent(widget, buffer, event);
-//    }
-
-    public static android.text.method.MovementMethod getInstance(Context c)
-    {
+    public static MovementMethod getInstance(Context c) {
         movementContext = c;
         return linkMovementMethod;
     }
-
 }
