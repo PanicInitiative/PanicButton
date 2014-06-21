abfc7a247a27435725ddbf6b1616deef37155555
Jun Matsushita <jun@iilab.org>
2014-06-21 19:00:49 +0200

    Allows pressing Done to validate Pin code entry #27

--- src/main/java/org/iilab/pb/LoginActivity.java
+++ src/main/java/org/iilab/pb/LoginActivity.java
@@ -5,11 +5,11 @@ import org.iilab.pb.common.ApplicationSettings;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.View.OnKeyListener;
 import android.widget.Button;
 import android.widget.EditText;
-
-
 
 public class LoginActivity extends PanicButtonActivity {
 
@@ -21,9 +21,34 @@ public class LoginActivity extends PanicButtonActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_screen_layout);
-
         passwordEditText = (EditText) findViewById(R.id.create_pin_edittext);
+        passwordEditText.setOnKeyListener(new OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                // If the event is a key-down event on the "enter" button
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                    (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                  // Perform action on key press
+                	String password = passwordEditText.getText().toString();
+                    if (ApplicationSettings.passwordMatches(getApplicationContext(), password)) {
 
+                        Intent i = new Intent(LoginActivity.this, MainActivity.class);
+//                        i = AppUtil.clearBackStack(i);
+                        if (ApplicationSettings.isAlertActive(LoginActivity.this)) {
+                            i.putExtra("page_id", "home-alerting");
+                        } else {
+                            i.putExtra("page_id", "home-ready");
+                        }
+                        startActivity(i);
+
+                        finish();
+                        return true;
+                    }
+                    AppUtil.setError(LoginActivity.this, passwordEditText, ((tryCount < 2) ? R.string.incorrect_pin : R.string.incorrect_pin_3_times));
+                    tryCount++;
+                }
+                return false;
+            }
+        });
         bAction = (Button) findViewById(R.id.b_action);
         bAction.setText(getResources().getString(R.string.code_ok));
         bAction.setOnClickListener(new View.OnClickListener() {
@@ -48,6 +73,8 @@ public class LoginActivity extends PanicButtonActivity {
                 tryCount++;
             }
         });
+        
+        
     }
     
 }
