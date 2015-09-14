a35ac201fa4458c936519874277b469f78902749
Neetika <neetikasood@gmail.com>
2015-09-14 11:03:23 +1000

    Fixed nested fragments access for Message UI Screen

--- app/src/main/java/org/iilab/pb/fragment/MessageTextFragment.java
+++ app/src/main/java/org/iilab/pb/fragment/MessageTextFragment.java
@@ -1,55 +1,59 @@
 package org.iilab.pb.fragment;
 
-import org.iilab.pb.R;
-import org.iilab.pb.common.MessageLimitWatcher;
-
-import android.app.Activity;
-import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.InputFilter;
-import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
+import org.iilab.pb.R;
+import org.iilab.pb.common.MessageLimitWatcher;
+import org.iilab.pb.common.MyTextView;
+import org.iilab.pb.model.SMSSettings;
 
 import static java.lang.String.valueOf;
 
 public class MessageTextFragment extends Fragment {
 
-    private TextView charactersLeftView;
-    private TextView messageHeaderView;
+    private MyTextView charactersLeftView;
+    private MyTextView messageHeaderView;
     private EditText messageEditText;
 
     private MessageLimitWatcher messageLimitWatcher;
-    private int maxCharacters;
-    private String messageHeader;
     private Button bAction;
-
-    @Override
-    public void onInflate(Activity activity, AttributeSet attrs, Bundle savedInstanceState) {
-        super.onInflate(activity, attrs, savedInstanceState);
-
-        TypedArray typedArray = activity.obtainStyledAttributes(attrs, R.styleable.MessageFragmentArguments);
-        maxCharacters = typedArray.getInt(R.styleable.MessageFragmentArguments_max_characters, -1);
-        messageHeader = typedArray.getString(R.styleable.MessageFragmentArguments_message_header);
-        typedArray.recycle();
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.message_fragment, container, false);
 
-        charactersLeftView = (TextView) view.findViewById(R.id.characters_left_view);
-        messageHeaderView = (TextView) view.findViewById(R.id.message_fragment_header);
+        charactersLeftView = (MyTextView) view.findViewById(R.id.characters_left_view);
+        messageHeaderView = (MyTextView) view.findViewById(R.id.message_fragment_header);
         messageEditText = (EditText) view.findViewById(R.id.message_edit_text);
         messageEditText.requestFocus();
+        View parentView = getParentFragment().getView();
+        if (parentView != null) {
+            bAction = (Button) parentView.findViewById(R.id.fragment_action);
+        }
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        String currentEmergencyMsg = SMSSettings.retrieveMessage(getActivity());
+        if (currentEmergencyMsg != null) {
+            displaySettings(currentEmergencyMsg);
+        }
+        setActionButtonStateListener(bAction);
+        bAction.setEnabled(!(messageEditText.getText().toString().trim().equals("")));
+        messageEditText.setImeOptions(EditorInfo.IME_FLAG_NAVIGATE_NEXT);
+        messageEditText.selectAll();
+
     }
 
     @Override
@@ -58,9 +62,9 @@ public class MessageTextFragment extends Fragment {
 
     }
 
-    public void setActionButtonStateListener(Button bAction){
+    public void setActionButtonStateListener(Button bAction) {
         this.bAction = bAction;
-
+        int maxCharacters = charactersLeftView.getMaxCharacters();
         String prefix = getString(R.string.characters_left);
         messageLimitWatcher = new MessageLimitWatcher(charactersLeftView, prefix, maxCharacters, bAction);
         messageEditText.addTextChangedListener(messageLimitWatcher);
@@ -69,7 +73,7 @@ public class MessageTextFragment extends Fragment {
         messageEditText.setSelection(messageEditText.getText().length());
 
         charactersLeftView.setText(prefix + valueOf(maxCharacters - messageEditText.getText().length()));
-        messageHeaderView.setText(messageHeader);
+        messageHeaderView.setText(messageHeaderView.getMessageHeader());
     }
 
     public String getMessage() {
@@ -80,5 +84,15 @@ public class MessageTextFragment extends Fragment {
         if (message != null) {
             messageEditText.setText(message);
         }
+    }
+
+    public void displaySettings(String msg) {
+        messageEditText.setText(msg);
+    }
+
+
+    public String getSMSSettingsFromView() {
+        String message = messageEditText.getText().toString().trim();
+        return message;
     }
 }
--- app/src/main/java/org/iilab/pb/fragment/SetupMessageFragment.java
+++ app/src/main/java/org/iilab/pb/fragment/SetupMessageFragment.java
@@ -4,16 +4,15 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.text.Html;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.EditorInfo;
 import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -37,12 +36,10 @@ import org.iilab.pb.model.SMSSettings;
  * Created by aoe on 12/12/13.
  */
 public class SetupMessageFragment extends Fragment {
-    private EditText smsEditText;
 
     private static final String PAGE_ID = "page_id";
     private static final String PARENT_ACTIVITY = "parent_activity";
     private Activity activity;
-
     DisplayMetrics metrics;
 
     TextView tvTitle, tvContent, tvIntro, tvWarning;
@@ -59,7 +56,7 @@ public class SetupMessageFragment extends Fragment {
         args.putString(PAGE_ID, pageId);
         args.putInt(PARENT_ACTIVITY, parentActivity);
         f.setArguments(args);
-        return(f);
+        return (f);
     }
 
 
@@ -72,23 +69,33 @@ public class SetupMessageFragment extends Fragment {
         tvIntro = (TextView) view.findViewById(R.id.fragment_intro);
         tvContent = (TextView) view.findViewById(R.id.fragment_contents);
 
+        MessageTextFragment childFragment = (MessageTextFragment) getChildFragmentManager().findFragmentById(R.id.sms_message);
+        if (childFragment == null) {
+            childFragment = new MessageTextFragment();
+            FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+            transaction.replace(R.id.sms_message, childFragment);
+            transaction.commit();
+        }
+
         bAction = (Button) view.findViewById(R.id.fragment_action);
         bAction.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.e(">>>>", "action button pressed");
-                String msg =  getSMSSettingsFromView();
+
+                MessageTextFragment childFragment = (MessageTextFragment) getChildFragmentManager().findFragmentById(R.id.sms_message);
+                String msg = childFragment.getSMSSettingsFromView();
 
                 SMSSettings.saveMessage(activity, msg);
-                displaySettings(msg);
+                childFragment.displaySettings(msg);
 
                 String pageId = currentPage.getAction().get(0).getLink();
                 int parentActivity = getArguments().getInt(PARENT_ACTIVITY);
                 Intent i;
 
-                if(parentActivity == AppConstants.FROM_WIZARD_ACTIVITY){
+                if (parentActivity == AppConstants.FROM_WIZARD_ACTIVITY) {
                     i = new Intent(activity, WizardActivity.class);
-                } else{
+                } else {
 //                	AppUtil.showToast("Message saved.", 1000, activity);
                     String confirmation = (currentPage.getAction().get(0).getConfirmation() == null)
                             ? AppConstants.DEFAULT_CONFIRMATION_MESSAGE
@@ -97,20 +104,18 @@ public class SetupMessageFragment extends Fragment {
 
                     i = new Intent(activity, MainActivity.class);
                 }
-                i.putExtra("page_id", pageId);
+                i.putExtra(PAGE_ID, pageId);
                 startActivity(i);
 
-                if(parentActivity == AppConstants.FROM_MAIN_ACTIVITY){
+                if (parentActivity == AppConstants.FROM_MAIN_ACTIVITY) {
                     activity.finish();
                 }
             }
         });
-
-
         lvItems = (ListView) view.findViewById(R.id.fragment_item_list);
 
         llWarning = (LinearLayout) view.findViewById(R.id.ll_fragment_warning);
-        tvWarning  = (TextView) view.findViewById(R.id.fragment_warning);
+        tvWarning = (TextView) view.findViewById(R.id.fragment_warning);
 
         lvItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
@@ -122,12 +127,12 @@ public class SetupMessageFragment extends Fragment {
                 int parentActivity = getArguments().getInt(PARENT_ACTIVITY);
                 Intent i;
 
-                if(parentActivity == AppConstants.FROM_WIZARD_ACTIVITY){
+                if (parentActivity == AppConstants.FROM_WIZARD_ACTIVITY) {
                     i = new Intent(activity, WizardActivity.class);
-                } else{
+                } else {
                     i = new Intent(activity, MainActivity.class);
                 }
-                i.putExtra("page_id", pageId);
+                i.putExtra(PAGE_ID, pageId);
                 startActivity(i);
 
             }
@@ -146,17 +151,6 @@ public class SetupMessageFragment extends Fragment {
             metrics = new DisplayMetrics();
             activity.getWindowManager().getDefaultDisplay().getMetrics(metrics);
 
-            Fragment fragment = getChildFragmentManager().findFragmentById(R.id.sms_message);
-            ((MessageTextFragment)fragment).setActionButtonStateListener(bAction);
-            smsEditText = (EditText) fragment.getView().findViewById(R.id.message_edit_text);
-
-            String currentMsg = SMSSettings.retrieveMessage(activity);
-            if(currentMsg != null) {
-                displaySettings(currentMsg);
-            }
-            bAction.setEnabled(!smsEditText.getText().toString().trim().equals(""));
-            smsEditText.setImeOptions(EditorInfo.IME_FLAG_NAVIGATE_NEXT);
-            smsEditText.selectAll();
             String pageId = getArguments().getString(PAGE_ID);
             String selectedLang = ApplicationSettings.getSelectedLanguage(activity);
 
@@ -167,17 +161,17 @@ public class SetupMessageFragment extends Fragment {
 
             tvTitle.setText(currentPage.getTitle());
 
-            if(currentPage.getContent() == null)
+            if (currentPage.getContent() == null)
                 tvContent.setVisibility(View.GONE);
             else
                 tvContent.setText(Html.fromHtml(currentPage.getContent(), null, new MyTagHandler()));
 
-            if(currentPage.getIntroduction() == null)
+            if (currentPage.getIntroduction() == null)
                 tvIntro.setVisibility(View.GONE);
             else
                 tvIntro.setText(currentPage.getIntroduction());
 
-            if(currentPage.getWarning() == null)
+            if (currentPage.getWarning() == null)
                 llWarning.setVisibility(View.GONE);
             else
                 tvWarning.setText(currentPage.getWarning());
@@ -193,14 +187,4 @@ public class SetupMessageFragment extends Fragment {
         }
     }
 
-
-    private void displaySettings(String msg) {
-        smsEditText.setText(msg);
-    }
-
-
-    private String getSMSSettingsFromView() {
-        String message = smsEditText.getText().toString().trim();
-        return message;
-    }
 }
