7448cefe2c17a2e0a51fa15583157bc79b9bd7c9
Jun Matsushita <jun@iilab.org>
2015-10-28 19:33:43 +0100

    Push to transifex works and translations seem correctly linked. #82

--- locales/locales-push.sh
+++ locales/locales-push.sh
@@ -7,13 +7,17 @@ do
   for f in panicbutton.io/_posts/$res/*.md
   do 
     echo "Processing $f file.."; 
-    cat $f | underscore --infmt text process 'data' | underscore process 'var lio = data.lastIndexOf("---"); data.slice(lio+1)' --outfmt text > ./build/$res/en/`basename $f`.txt
+    cat $f | underscore --infmt text process 'data' | underscore process 'var lio = data.lastIndexOf("---"); var slc = data.slice(lio+1); if (slc.every(function(val){return val==""})) { } else { slc }' --outfmt text > ./build/$res/en/`basename $f`.txt
     echo "./build/$res/en/`basename $f`.txt file created."; 
     cat $f | underscore --infmt text process 'data' | underscore process 'var lio = data.lastIndexOf("---"); var io =  data.indexOf("---"); if (lio != io) {data.slice(io+1,lio)} else {data.slice(0,lio)}' --outfmt text | yaml2json - | jq '.' > ./build/$res/en/`basename $f`.json.orig
     echo "./build/$res/en/`basename $f`.json.orig file created."; 
     cat $f | underscore --infmt text process 'data' | underscore process 'var lio = data.lastIndexOf("---"); var io =  data.indexOf("---"); if (lio != io) {data.slice(io+1,lio)} else {data.slice(0,lio)}' --outfmt text | yaml2json - | jq --arg file `basename $f` -f pre-tx-push.jq > ./build/$res/en/`basename $f`.json.tmp
     echo "./build/$res/en/`basename $f`.json.tmp file created."; 
-    jq '$json[0] * { ("content-" + $file): . }' --arg file `basename $f` --slurpfile json ./build/$res/en/`basename $f`.json.tmp -sR ./build/$res/en/`basename $f`.txt > ./build/$res/en/`basename $f`.json
+    if [[ -s ./build/$res/en/`basename $f`.txt ]] ; then
+      jq '$json[0] * { ("content-" + $file): . }' --arg file `basename $f` --slurpfile json ./build/$res/en/`basename $f`.json.tmp -sR ./build/$res/en/`basename $f`.txt > ./build/$res/en/`basename $f`.json
+    else
+      cp ./build/$res/en/`basename $f`.json.tmp ./build/$res/en/`basename $f`.json
+    fi;      
     echo "./build/$res/en/`basename $f`.json file created."; 
   done
 
@@ -26,7 +30,7 @@ do
     for f in panicbutton.io/_posts/$res/$lang/*.md
     do 
       echo "Processing $f file.."; 
-      cat $f | underscore --infmt text process 'data' | underscore process 'var lio = data.lastIndexOf("---"); data.slice(lio+1)' --outfmt text > ./build/$res/$lang/`basename $f`.txt
+      cat $f | underscore --infmt text process 'data' | underscore process 'var lio = data.lastIndexOf("---"); var slc = data.slice(lio+1); if (slc.every(function(val){return val==""})) { } else { slc }' --outfmt text > ./build/$res/$lang/`basename $f`.txt
       echo "./build/$res/$lang/`basename $f`.txt file created."; 
       cat $f | underscore --infmt text process 'data' | underscore process 'var lio = data.lastIndexOf("---"); var io =  data.indexOf("---"); if (lio != io) {data.slice(io+1,lio)} else {data.slice(0,lio)}' --outfmt text | yaml2json - | jq '.' > ./build/$res/$lang/`basename $f`.json.orig
       echo "./build/$res/$lang/`basename $f`.json.orig file created."; 
@@ -39,3 +43,4 @@ do
     jq 'add' -s build/$res/$lang/*.json > ./$res.$lang.json
   done
 done
+tx push -s -t
