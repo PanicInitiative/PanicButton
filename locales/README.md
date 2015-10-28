2dd2341900206fc9ef64e9b3f868c1ae04f9e00b
Jun Matsushita <jun@iilab.org>
2015-10-28 12:57:24 +0100

    Basics for generating split files from markdown with frontmatter. Working on jq transform

---
+++ locales/README.md
@@ -0,0 +1,16 @@
+# Tools
+
+These are tools related to the build process.
+
+## Install
+ - [jq](https://stedolan.github.io/jq/download/)
+ - [nodejs](https://nodejs.org/en/download/)
+ - underscore-cli ```npm install -g underscore-cli```
+
+## Run
+
+
+```
+git clone --depth=1 https://github.com/PanicInitiative/panicbutton.io
+yaml-front-matter  | jq -f pre-tx-push.jq | 
+
---
+++ locales/locales-push.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#git clone --depth=1 https://github.com/PanicInitiative/panicbutton.io
+#rm -rf ./build
+#mkdir ./build
+for f in panicbutton.io/_posts/mobile/*.md
+do 
+  echo "Processing $f file.."; 
+  cat $f | underscore --infmt text process 'data' | underscore process 'var lio = data.lastIndexOf("---"); data.slice(lio+1)' --outfmt text > ./build/`basename $f`.txt
+  echo "./build/`basename $f`.txt file created."; 
+  cat $f | underscore --infmt text process 'data' | underscore process 'var lio = data.lastIndexOf("---"); var io =  data.indexOf("---"); if (lio != io) {data.slice(io+1,lio)} else {data.slice(0,lio)}' --outfmt text | yaml2json - | jq '.' > ./build/`basename $f`.json
+  echo "./build/`basename $f`.json file created."; 
+done
+
