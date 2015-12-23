1e69bdf786bb063165b3bfa9f5c8e5e6431c3dc1
Hans-Christoph Steiner <hans@eds.org>
2015-12-23 10:23:21 +0100

    show all Panic Responder apps so the user can enable/disable them
    This shows all apps that will responder to the panic trigger, and provides
    a switch each app so the user can enable/disable which apps should receive
    the trigger.

--- app/src/main/java/org/iilab/pb/fragment/PanicRespondersFragment.java
+++ app/src/main/java/org/iilab/pb/fragment/PanicRespondersFragment.java
@@ -1,13 +1,26 @@
 package org.iilab.pb.fragment;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import org.iilab.pb.R;
@@ -16,9 +29,15 @@ import org.iilab.pb.common.ApplicationSettings;
 import org.iilab.pb.data.PBDatabase;
 import org.iilab.pb.model.Page;
 
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
+
+import info.guardianproject.panic.PanicTrigger;
 
 public class PanicRespondersFragment extends Fragment {
+    public static final String TAG = "PanicRespondersFragment";
 
     private static final String PAGE_ID = "page_id";
     private static final String PARENT_ACTIVITY = "parent_activity";
@@ -27,6 +46,19 @@ public class PanicRespondersFragment extends Fragment {
     TextView tvTitle, tvIntro;
     Page currentPage;
     PageItemAdapter pageItemAdapter;
+
+    private static final int CONNECT_RESULT = 0x01;
+
+    private String responders[];
+    private Set<String> enabledResponders;
+    private Set<String> respondersThatCanConnect;
+    private ArrayList<CharSequence> appLabelList;
+    private ArrayList<Drawable> iconList;
+
+    private SharedPreferences prefs;
+
+    private String requestPackageName;
+    private String requestAction;
 
     public static PanicRespondersFragment newInstance(String pageId, int parentActivity) {
         PanicRespondersFragment f = new PanicRespondersFragment();
@@ -45,6 +77,63 @@ public class PanicRespondersFragment extends Fragment {
         tvIntro = (TextView) view.findViewById(R.id.fragment_intro);
 
         return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        enabledResponders = PanicTrigger.getEnabledResponders(activity);
+        respondersThatCanConnect = PanicTrigger.getRespondersThatCanConnect(activity);
+
+        // sort enabled first, then disabled
+        LinkedHashSet<String> a = new LinkedHashSet<String>(enabledResponders);
+        LinkedHashSet<String> b = new LinkedHashSet(PanicTrigger.getAllResponders(activity));
+        b.removeAll(enabledResponders);
+        a.addAll(b);
+        responders = a.toArray(new String[a.size()]);
+
+        PackageManager pm = getActivity().getPackageManager();
+        appLabelList = new ArrayList<CharSequence>(responders.length);
+        iconList = new ArrayList<Drawable>(responders.length);
+        for (String packageName : responders) {
+            try {
+                appLabelList.add(pm.getApplicationLabel(pm.getApplicationInfo(packageName, 0)));
+                iconList.add(pm.getApplicationIcon(packageName));
+            } catch (PackageManager.NameNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        RecyclerView recyclerView = (RecyclerView) activity.findViewById(R.id.recycler_view);
+        recyclerView.addItemDecoration(new SimpleDividerItemDecoration(activity));
+        recyclerView.setHasFixedSize(true); // does not change, except in onResume()
+        recyclerView.setLayoutManager(new LinearLayoutManager(activity));
+        recyclerView.setAdapter(new RecyclerView.Adapter<AppRowHolder>() {
+            @Override
+            public AppRowHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                Context context = parent.getContext();
+                LayoutInflater layoutInflater = LayoutInflater.from(context);
+                View view = layoutInflater.inflate(R.layout.responder_row, parent, false);
+                return new AppRowHolder(view);
+            }
+
+            @Override
+            public void onBindViewHolder(AppRowHolder holder, int position) {
+                String packageName = responders[position];
+                boolean canConnect = respondersThatCanConnect.contains(packageName);
+                holder.setupForApp(
+                        packageName,
+                        iconList.get(position),
+                        appLabelList.get(position),
+                        canConnect);
+            }
+
+            @Override
+            public int getItemCount() {
+                return appLabelList.size();
+            }
+        });
     }
 
     @Override
@@ -84,6 +173,91 @@ public class PanicRespondersFragment extends Fragment {
             for (Fragment fragment : fragments) {
                 fragment.onActivityResult(requestCode, resultCode, intent);
             }
+        }
+    }
+
+
+    class AppRowHolder extends RecyclerView.ViewHolder {
+
+        private final View.OnClickListener onClickListener;
+        private final Switch onSwitch;
+        private final TextView editableLabel;
+        private final ImageView iconView;
+        private final TextView appLabelView;
+        private String rowPackageName;
+
+        AppRowHolder(final View row) {
+            super(row);
+
+            iconView = (ImageView) row.findViewById(R.id.iconView);
+            appLabelView = (TextView) row.findViewById(R.id.appLabel);
+            editableLabel = (TextView) row.findViewById(R.id.editableLabel);
+            onSwitch = (Switch) row.findViewById(R.id.on_switch);
+            onClickListener = new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    requestPackageName = rowPackageName;
+                    Intent intent = new Intent(requestAction);
+                    intent.setPackage(requestPackageName);
+                    startActivityForResult(intent, CONNECT_RESULT);
+                }
+            };
+
+            onSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton compoundButton, boolean enabled) {
+                    setEnabled(enabled);
+                    if (enabled) {
+                        PanicTrigger.enableResponder(activity, rowPackageName);
+                    } else {
+                        PanicTrigger.disableResponder(activity, rowPackageName);
+                    }
+                }
+            });
+        }
+
+        void setEnabled(boolean enabled) {
+            if (enabled) {
+                editableLabel.setVisibility(View.VISIBLE);
+                appLabelView.setEnabled(true);
+                iconView.setEnabled(true);
+                iconView.setColorFilter(null);
+            } else {
+                editableLabel.setVisibility(View.GONE);
+                appLabelView.setEnabled(false);
+                iconView.setEnabled(false);
+                // grey out app icon when disabled
+                ColorMatrix matrix = new ColorMatrix();
+                matrix.setSaturation(0);
+                ColorMatrixColorFilter filter = new ColorMatrixColorFilter(matrix);
+                iconView.setColorFilter(filter);
+            }
+        }
+
+        void setupForApp(String packageName, Drawable icon, CharSequence appLabel, boolean editable) {
+            this.rowPackageName = packageName;
+            iconView.setImageDrawable(icon);
+            appLabelView.setText(appLabel);
+            if (editable) {
+                iconView.setOnClickListener(onClickListener);
+                appLabelView.setOnClickListener(onClickListener);
+                editableLabel.setOnClickListener(onClickListener);
+                editableLabel.setText(R.string.edit);
+                editableLabel.setTypeface(null, Typeface.BOLD);
+                if (Build.VERSION.SDK_INT >= 14)
+                    editableLabel.setAllCaps(true);
+            } else {
+                iconView.setOnClickListener(null);
+                appLabelView.setOnClickListener(null);
+                editableLabel.setOnClickListener(null);
+                editableLabel.setText(R.string.app_hides);
+                editableLabel.setTypeface(null, Typeface.NORMAL);
+                if (Build.VERSION.SDK_INT >= 14)
+                    editableLabel.setAllCaps(false);
+            }
+            boolean enabled = enabledResponders.contains(packageName);
+            onSwitch.setChecked(enabled);
+            setEnabled(enabled);
         }
     }
 
