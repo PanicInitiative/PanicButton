e6e61a02ee782679da3aa28d6d1e5ef13f7d229c
Neetika <neetikasood@gmail.com>
2016-03-21 12:25:11 +1100

    #178 changes to handle the Settings subscreen for advanced trigger settings

--- app/src/main/java/org/iilab/pb/HomeActivity.java
+++ app/src/main/java/org/iilab/pb/HomeActivity.java
@@ -114,22 +114,21 @@ public class HomeActivity extends AppCompatActivity {
 
 		/* We update the device language content if the english mobile_en.json version has increased or
         * if the language of the device OS was previously not installed*/
-        Log.d(TAG, "currentLocalContentVersion is" +currentLocalContentVersion +"and  newLocalContentVersion is "+ newLocalContentVersion);
+        Log.d(TAG, "currentLocalContentVersion is" + currentLocalContentVersion + "and  newLocalContentVersion is " + newLocalContentVersion);
         if ((newLocalContentVersion > currentLocalContentVersion) || (!isLanguageDataExists(getApplicationContext(), selectedLang))) {
             Log.d(TAG, "Update local data as the english mobile_en.json version has increased");
             // to check if the app is installed first time, -1 is the default value
-            if(currentLocalContentVersion== FRESH_INSTALL_APP_RELEASE_NO){
-                setFirstRun(getApplicationContext(),true);
+            if (currentLocalContentVersion == FRESH_INSTALL_APP_RELEASE_NO) {
+                setFirstRun(getApplicationContext(), true);
                 setAppUpdated(getApplicationContext(), false);
-            }else {
-                setFirstRun(getApplicationContext(),false);
+            } else {
+                setFirstRun(getApplicationContext(), false);
                 setAppUpdated(getApplicationContext(), true);
             }
             new InitializeLocalData().execute();
             addDBLoadedLanguage(getApplicationContext(), selectedLang);
 
-        }
-        else {
+        } else {
             Log.d(TAG, "no update of local data needed");
             startNextActivity();
         }
@@ -174,22 +173,25 @@ public class HomeActivity extends AppCompatActivity {
     private void startNextActivity() {
         Log.d(TAG, "starting next activity");
         int wizardState = getWizardState(this);
-        supportedLangs = getSupportedLanguages(this);
-        Log.d(TAG, "Checking supported languages " + supportedLangs);
-        if (null == supportedLangs) {
-            PBDatabase dbInstance = new PBDatabase(this);
-            dbInstance.open();
-            Page languagesPage = dbInstance.retrievePage(PAGE_SETUP_LANGUAGE, DEFAULT_LANGUAGE_ENG);
-            setSupportedLanguages(languagesPage);
-            dbInstance.close();
-        }
-        if ((supportedLangs == null) || !(supportedLangs.contains(selectedLang))) {
-            setSelectedLanguage(this, DEFAULT_LANGUAGE_ENG);
-        }
+
         //TODO remove after testing
-//        wizardState=WIZARD_FLAG_HOME_READY;
+//        wizardState = WIZARD_FLAG_HOME_READY;
         if (wizardState != WIZARD_FLAG_HOME_READY) {
             Log.d(TAG, "First run TRUE, running WizardActivity with pageId = " + pageId);
+
+            supportedLangs = getSupportedLanguages(this);
+            Log.d(TAG, "Checking supported languages " + supportedLangs);
+            if (null == supportedLangs) {
+                PBDatabase dbInstance = new PBDatabase(this);
+                dbInstance.open();
+                Page languagesPage = dbInstance.retrievePage(PAGE_SETUP_LANGUAGE, DEFAULT_LANGUAGE_ENG);
+                setSupportedLanguages(languagesPage);
+                dbInstance.close();
+            }
+            if ((supportedLangs == null) || !(supportedLangs.contains(selectedLang))) {
+                setSelectedLanguage(this, DEFAULT_LANGUAGE_ENG);
+            }
+
             Intent i = new Intent(HomeActivity.this, WizardActivity.class);
             i.putExtra(PAGE_ID, pageId);
             startActivity(i);
@@ -197,7 +199,7 @@ public class HomeActivity extends AppCompatActivity {
             Log.d(TAG, "First run FALSE, running CalculatorActivity");
             Intent i = new Intent(HomeActivity.this, CalculatorActivity.class);
             // Make sure the HardwareTriggerService is started
-            if(isHardwareTriggerServiceEnabled(this)) {
+            if (isHardwareTriggerServiceEnabled(this)) {
                 startService(new Intent(this, HardwareTriggerService.class));
             }
             startActivity(i);
@@ -256,184 +258,7 @@ public class HomeActivity extends AppCompatActivity {
                     Log.e(TAG, "Exception while dismissing progress dialog " + e.getMessage());
                     e.printStackTrace();
                 }
-
-//            setLocalDataInsertion(HomeActivity.this, true);
-//            setLastUpdatedDBVersion(HomeActivity.this, DATABASE_VERSION);
-
             startNextActivity();
         }
     }
-
-//    private class GetLatestVersion extends AsyncTask<Void, Void, Boolean> {
-//
-//        @Override
-//        protected void onPreExecute() {
-//            super.onPreExecute();
-//            pDialog = ProgressDialog.show(HomeActivity.this, "Application", "Starting...", true, false);
-//        }
-//
-//        @Override
-//        protected Boolean doInBackground(Void... params) {
-//
-//            String url = AppConstants.BASE_URL + AppConstants.VERSION_CHECK_URL;
-//            JsonParser jsonParser = new JsonParser();
-//            ServerResponse response = jsonParser.retrieveServerData(AppConstants.HTTP_REQUEST_TYPE_GET, url, null, null, null);
-//            if (response.getStatus() == 200) {
-//                try {
-//                    JSONObject responseObj = response.getjObj();
-//                    latestVersion = responseObj.getInt("version");
-//                    Log.e("??????", "latest version = " + latestVersion + " last updated version = " + lastUpdatedVersion);
-//                    return true;
-//                } catch (JSONException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//            return false;
-//        }
-//
-//        @Override
-//        protected void onPostExecute(Boolean response) {
-//            super.onPostExecute(response);
-//
-//            if (latestVersion > lastUpdatedVersion) {
-//                new GetMobileDataUpdate().execute();
-//            } else {
-//                setLastRunTimeInMillis(HomeActivity.this, System.currentTimeMillis());
-//                if (pDialog.isShowing())
-//					try {
-//						pDialog.dismiss();
-//					} catch (Exception e) {
-//						e.printStackTrace();
-//					}
-//                    startNextActivity();
-//            }
-//        }
-//    }
-
-
-//    private class GetMobileDataUpdate extends AsyncTask<Void, Void, Boolean> {
-//
-//        @Override
-//        protected void onPreExecute() {
-//            super.onPreExecute();
-//            try {
-//				pDialog = ProgressDialog.show(HomeActivity.this, "Application", "Downloading updates...", true, false);
-//			} catch (Exception e) {
-//				e.printStackTrace();
-//			}
-//       }
-//
-//        @Override
-//        protected Boolean doInBackground(Void... params) {
-//
-//            int version = 0;
-//            for(version = lastUpdatedVersion + 1; version <= latestVersion; version ++){
-//                if (selectedLang.equals("en")) {
-//                    mobileDataUrl = AppConstants.BASE_URL + "/api/mobile." + version + ".json";
-//                } else {
-//                    mobileDataUrl = AppConstants.BASE_URL + "/api/" + selectedLang + "/" + "mobile." + version + ".json";
-//                }
-//
-//                JsonParser jsonParser = new JsonParser();
-//                ServerResponse response = jsonParser.retrieveServerData(AppConstants.HTTP_REQUEST_TYPE_GET, mobileDataUrl, null, null, null);
-//                if (response.getStatus() == 200) {
-//                    Log.d(">>>><<<<", "success in retrieving server-response for url = " + mobileDataUrl);
-//                    try {
-//                        JSONObject responseObj = response.getjObj();
-//                        JSONObject mobObj = responseObj.getJSONObject(JSON_OBJECT_MOBILE);
-//                        JSONArray dataArray = mobObj.getJSONArray(JSON_ARRAY_DATA);
-//                        insertMobileDataToLocalDB(dataArray);
-//                    } catch (JSONException e) {
-//                        e.printStackTrace();
-//                        return false;
-//                    }
-//                }
-//            }
-//
-//            if(version > latestVersion){
-//                return true;
-//            } else{
-//                return false;
-//            }
-//        }
-//
-//        @Override
-//        protected void onPostExecute(Boolean response) {
-//            super.onPostExecute(response);
-//
-//            if(response){
-//                new GetHelpDataUpdate().execute();
-//            }
-//            else{
-//                if (pDialog.isShowing())
-//                    pDialog.dismiss();
-//
-//                startNextActivity();
-//            }
-//        }
-//    }
-
-
-//    private class GetHelpDataUpdate extends AsyncTask<Void, Void, Boolean> {
-//
-//        @Override
-//        protected void onPreExecute() {
-//            super.onPreExecute();
-//            try {
-//				pDialog = ProgressDialog.show(HomeActivity.this, "Application", "Downloading help pages...", true, false);
-//			} catch (Exception e) {
-//				e.printStackTrace();
-//			}
-//        }
-//
-//        @Override
-//        protected Boolean doInBackground(Void... params) {
-//
-//            JsonParser jsonParser = new JsonParser();
-//            ServerResponse response = jsonParser.retrieveServerData(AppConstants.HTTP_REQUEST_TYPE_GET, helpDataUrl, null, null, null);
-//            if (response.getStatus() == 200) {
-//                Log.d(">>>><<<<", "success in retrieving server-response for url = " + helpDataUrl);
-//                setLastRunTimeInMillis(HomeActivity.this, System.currentTimeMillis());          // if we can retrieve a single data, we change it up-to-date
-//                try {
-//                    JSONObject responseObj = response.getjObj();
-//                    JSONObject mobObj = responseObj.getJSONObject(JSON_OBJECT_HELP);
-//                    JSONArray dataArray = mobObj.getJSONArray(JSON_ARRAY_DATA);
-//                    insertHelpDataToLocalDB(dataArray);
-//                    setLastUpdatedVersion(HomeActivity.this, latestVersion);
-//                    return true;
-//                } catch (JSONException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//            return false;
-//        }
-//
-//        @Override
-//        protected void onPostExecute(Boolean response) {
-//            super.onPostExecute(response);
-//            if (pDialog.isShowing())
-//				try {
-//					pDialog.dismiss();
-//				} catch (Exception e) {
-//					e.printStackTrace();
-//				}
-//
-//            startNextActivity();
-//        }
-//    }
-
-
-//    private void insertHelpDataToLocalDB(JSONArray dataArray) {
-//        List<HelpPage> pageList = HelpPage.parseHelpPages(dataArray);
-//
-//        PBDatabase dbInstance = new PBDatabase(HomeActivity.this);
-//        dbInstance.open();
-//
-//        for (int i = 0; i < pageList.size(); i++) {
-//            dbInstance.insertOrUpdateHelpPage(pageList.get(i));
-//        }
-//        dbInstance.close();
-//    }
-
-
 }
---
+++ app/src/main/java/org/iilab/pb/fragment/AdvancedSettingsSubScreenFragment.java
@@ -0,0 +1,72 @@
+package org.iilab.pb.fragment;
+
+import android.os.Bundle;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceFragmentCompat;
+import android.util.Log;
+
+import org.iilab.pb.R;
+
+import static org.iilab.pb.common.AppConstants.ALARM_SENDING_CONFIRMATION_PATTERN_LONG;
+import static org.iilab.pb.common.AppConstants.ALARM_SENDING_CONFIRMATION_PATTERN_NONE;
+import static org.iilab.pb.common.AppConstants.PAGE_ID;
+import static org.iilab.pb.common.AppConstants.PARENT_ACTIVITY;
+import static org.iilab.pb.common.ApplicationSettings.isAlarmConfirmationRequired;
+import static org.iilab.pb.common.ApplicationSettings.setAlarmConfirmationRequired;
+import static org.iilab.pb.common.ApplicationSettings.setConfirmationFeedbackVibrationPattern;
+
+public class AdvancedSettingsSubScreenFragment extends PreferenceFragmentCompat {
+    private static final String TAG = AdvancedSettingsSubScreenFragment.class.getName();
+
+    public static AdvancedSettingsSubScreenFragment newInstance(String pageId, int parentActivity) {
+        AdvancedSettingsSubScreenFragment f = new AdvancedSettingsSubScreenFragment();
+        Bundle args = new Bundle();
+        args.putString(PAGE_ID, pageId);
+        args.putInt(PARENT_ACTIVITY, parentActivity);
+        f.setArguments(args);
+        return (f);
+    }
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        // Load the preferences from an XML resource
+        setPreferencesFromResource(R.xml.preferences, rootKey);
+        Log.d(TAG, "onCreatePreferences of the sub screen "+rootKey);
+        Preference alertConfirmationSettings = (Preference) findPreference(getString(R.string.confirmationSequenceKey));
+        if (isAlarmConfirmationRequired(getActivity())) {
+            enableConfirmationPatterns(true);
+        } else {
+            enableConfirmationPatterns(false);
+        }
+        alertConfirmationSettings.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object selectedValue) {
+                Log.d(TAG, "Inside of Alarm confirmation settings");
+
+                if (selectedValue.equals(getString(R.string.confirmationSequenceDefault))) {
+                    // disable Confirmation Wait Time/ Confirmation Wait Vibration
+                    enableConfirmationPatterns(false);
+                    setAlarmConfirmationRequired(getActivity(), false);
+                    setConfirmationFeedbackVibrationPattern(getActivity(), ALARM_SENDING_CONFIRMATION_PATTERN_NONE);
+                    Log.d(TAG, "default confirmation press deactivated");
+                } else {
+                    // enable Confirmation Wait Time/ Confirmation Wait Vibration
+                    enableConfirmationPatterns(true);
+                    setAlarmConfirmationRequired(getActivity(), true);
+                    setConfirmationFeedbackVibrationPattern(getActivity(), ALARM_SENDING_CONFIRMATION_PATTERN_LONG);
+                    Log.d(TAG, "Confirmation press enabled");
+                }
+                return true;
+            }
+        });
+    }
+
+    private void enableConfirmationPatterns(boolean flag) {
+        Preference confirmationWaitTime = (Preference) findPreference(getString(R.string.confirmationWaitTimeKey));
+        confirmationWaitTime.setEnabled(flag);
+        Preference confirmationWaitVibration = (Preference) findPreference(getString(R.string.hapticFeedbackVibrationPatternKey));
+        confirmationWaitVibration.setEnabled(flag);
+    }
+}
+
